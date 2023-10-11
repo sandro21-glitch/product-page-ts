181a03955a50bbbfff2d8e0d54835c421f678698
Sandro <vipsandro11@gmail.com>
2023-10-11 18:02:03 +0400

    feat: Create CartItems component for shopping cart functionality

---
+++ src/features/CartItems/CartItems.tsx
@@ -0,0 +1,32 @@
+import cartImage from "../../assets/image-product-1.jpg";
+import removeItem from "../../assets/icon-delete.svg";
+type Props = {};
+
+const CartItems = () => {
+  return (
+    <div className="p-5">
+      <div className="flex justify-between">
+        <div className="flex gap-5 mb-5">
+          <img
+            src={cartImage}
+            alt="product image"
+            className="max-w-[4rem] h-auto"
+          />
+          <div className="flex flex-col justify-evenly">
+            <h6>Fall Limited Edition Sneakers</h6>
+            <p>
+              $125.00 x 3 <span className="font-bold">$375.00</span>
+            </p>
+          </div>
+        </div>
+        <button>
+          <img src={removeItem} alt="delete item" />
+        </button>
+      </div>
+      <button className="w-full font-bold bg-NormalOrange p-5 rounded-lg text-white">
+        Checkout
+      </button>
+    </div>
+  );
+};
+export default CartItems;
