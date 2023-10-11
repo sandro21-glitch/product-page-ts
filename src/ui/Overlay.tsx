8a998d44daf0acf061cc2441c2c99fd6db9fca0a
Sandro <vipsandro11@gmail.com>
2023-10-11 18:05:57 +0400

    feat: Enhance Overlay component with responsive behavior

--- src/ui/Overlay.tsx
+++ src/ui/Overlay.tsx
@@ -6,7 +6,7 @@ const Overlay = ({ isOpen }: OverlayType) => {
     <div
       className={`${
         isOpen ? "opacity-[75%]" : "opacity-0 hidden"
-      }  bg-Black w-full h-full fixed left-0 top-0 bottom-0 right-0 z-[1]`}
+      }  bg-Black w-full h-full fixed block md:hidden left-0 top-0 bottom-0 right-0 z-[1]`}
     ></div>
   );
 };
