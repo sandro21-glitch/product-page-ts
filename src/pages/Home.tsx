400786c674e9d467e4f0a0abb64c64ffd147badb
Sandro <vipsandro11@gmail.com>
2023-10-12 22:30:59 +0400

    refactor: Update layout in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import ProductInfo from "../features/Home/ProductInfo";
 const Home = () => {
   return (
     <section className="py-20">
-      <article className="section-center flex justify-evenly">
+      <article className="section-center p-5 gap-5 flex flex-col md:flex-row justify-center md:justify-evenly">
         <ProductImages />
         <ProductInfo />
       </article>
