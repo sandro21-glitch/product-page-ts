57a2f93c764b313cc8fe661f99fccf9766347b2b
Sandro <vipsandro11@gmail.com>
2023-10-14 20:03:18 +0400

    refactor: Update Home component to accept cart-related props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,24 @@
 import ProductImages from "../features/Home/ProductImages";
 import ProductInfo from "../features/Home/ProductInfo";
 
-const Home = () => {
+type CartTyps = {
+  setAmount: (amount: number) => void;
+  amount: number;
+  price: number;
+  handleAddCart: (price: number, amount: number) => void;
+};
+
+const Home = ({ setAmount, amount, price, handleAddCart }: CartTyps) => {
   return (
-    <section className="py-20">
+    <section className="py-20 relative">
       <article className="section-center p-5 gap-5 flex flex-col md:flex-row justify-center md:justify-evenly">
         <ProductImages />
-        <ProductInfo />
+        <ProductInfo
+          setAmount={setAmount}
+          amount={amount}
+          price={price}
+          handleAddCart={handleAddCart}
+        />
       </article>
     </section>
   );
