ed8fced002dafe253f33a0f1eeac799c06b3a4d9
Sandro <vipsandro11@gmail.com>
2023-10-14 20:02:15 +0400

    feat: Enhance CartPopup component with dynamic cart content and handling

--- src/ui/CartPopup.tsx
+++ src/ui/CartPopup.tsx
@@ -1,17 +1,19 @@
 import CartItems from "../features/CartItems/CartItems";
 
 type CartPopupType = {
+  handleRemoveItem: () => void;
   openCart: boolean;
+  cartItem: { name: string; amount: number; price: number };
 };
 
-const CartPopup = ({ openCart }: CartPopupType) => {
+const CartPopup = ({ openCart, cartItem, handleRemoveItem }: CartPopupType) => {
   return (
     <div
       className={`
         ${openCart ? "block" : "hidden"} rounded-md 
         
         absolute right-0 md:right-0 left-0 md:left-auto border border-Grayishblue top-full
-        w-[90%] m-auto bg-white
+        w-[90%] m-auto bg-white z-[1]
         md:w-[25rem] h-[15rem] md:h-13 shadow-ShadowBottom
         `}
     >
@@ -19,7 +21,13 @@ const CartPopup = ({ openCart }: CartPopupType) => {
         <h6>Cart</h6>
       </div>
       {/* cart content */}
-      <CartItems />
+      {cartItem.amount < 1 ? (
+        <p className="flex items-center justify-center mt-14 font-bold text-Darkgrayishblue">
+          Your cart is empty.
+        </p>
+      ) : (
+        <CartItems cartItem={cartItem} handleRemoveItem={handleRemoveItem} />
+      )}
     </div>
   );
 };
