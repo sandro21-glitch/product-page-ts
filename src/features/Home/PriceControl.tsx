781adcd03f41c6f9520cd7a20455743a46bdb5f0
Sandro <vipsandro11@gmail.com>
2023-10-14 20:04:21 +0400

    refactor: Update PriceControl component for cart integration and responsive design

--- src/features/Home/PriceControl.tsx
+++ src/features/Home/PriceControl.tsx
@@ -1,32 +1,51 @@
-import { useState } from "react";
 import minusImg from "../../assets/icon-minus.svg";
 import plusImg from "../../assets/icon-plus.svg";
-import AddToCartBtn from "../../ui/AddToCartBtn";
+import AddToCart from "../../ui/AddToCart";
+type AmountType = {
+  setAmount: (amount: number) => void;
+  amount: number;
+  price: number;
+  handleAddCart: (price: number, amount: number) => void;
+};
+const PriceControl = ({
+  setAmount,
+  amount,
+  price,
+  handleAddCart,
+}: AmountType) => {
+  const handleIncreaseAmount = (): void => {
+    setAmount(amount + 1);
+  };
+  const handleDecreaseAmount = (): void => {
+    if (amount > 1) {
+      setAmount(amount - 1);
+    }
+  };
 
-const PriceControl = () => {
-  const [price, setPrice] = useState<number>(125);
-  const [amount, setAmount] = useState<number>(0);
   return (
     <div>
-      <div className="mb-5">
-        <h4 className="font-bold text-Verydarkblue">
-          ${price}.00{" "}
-          <span className="text-sm text-NormalOrange ml-3">50%</span>
-        </h4>
+      <div className="mb-5 flex items-center justify-between md:block">
+        <h3 className="font-bold text-Verydarkblue">
+          $125.00 <span className="text-sm bg-PaleOrange text-NormalOrange ml-3 px-2 py-1 rounded-full">50%</span>
+        </h3>
         <del className="font-bold text-Grayishblue">$250.00</del>
       </div>
       {/* price control */}
-      <div className="flex items-center justify-between">
-        <div className="flex items-center justify-between w-[7rem]">
-          <button>
-            <img src={minusImg} alt="" />
+      <div className="flex items-center flex-col md:flex-row md:justify-between gap-10 md:gap-14">
+        <div className="flex items-center justify-between w-full md:w-[8rem] p-5 bg-Lightgrayishblue rounded-md">
+          <button onClick={handleDecreaseAmount}>
+            <img src={minusImg} alt="minusImg" />
           </button>
           <span className="font-bold text-Verydarkblue">{amount}</span>
-          <button>
-            <img src={plusImg} alt="" />
+          <button onClick={handleIncreaseAmount}>
+            <img src={plusImg} alt="plusImg" />
           </button>
         </div>
-        <AddToCartBtn />
+        <AddToCart
+          handleAddCart={handleAddCart}
+          price={price}
+          amount={amount}
+        />
       </div>
     </div>
   );
